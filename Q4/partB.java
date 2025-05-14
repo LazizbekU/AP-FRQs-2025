5883ff14b28b03d3afbfbe423c8a0fd12b3eda44
LazizbekU <lazizbeku@nycstudents.net>
2025-05-14 18:35:19 -0400

    Update partB.java

--- Q4/partB.java
+++ Q4/partB.java
@@ -1,1 +1,13 @@
-
+public boolean clearPair(int row, int col) {
+    int targetValue = puzzle[row][col];
+    for (int i = row; i < puzzle.length; i++) {
+        for (int j = (i == row) ? col + 1 : 0; j < puzzle[i].length; j++) {
+            if (puzzle[i][j] != 0 && (puzzle[i][j] == targetValue || puzzle[i][j] + targetValue == 10)) {
+                puzzle[row][col] = 0;
+                puzzle[i][j] = 0;
+                return true;
+            }
+        }
+    }
+    return false;
+}
