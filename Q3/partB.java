fb2a3c726089d30051c85575d652a18fd0f47bf3
LazizbekU <lazizbeku@nycstudents.net>
2025-05-14 18:27:46 -0400

    Update partB.java

--- Q3/partB.java
+++ Q3/partB.java
@@ -1,1 +1,19 @@
+public ArrayList<Match> buildMatches() {
+    ArrayList<Match> matches = new ArrayList<Match>();
+    int start = 0;
+    if (competitorList.size() % 2 == 0) {
+        start = 0;
+    } else {
+        start = 1;
+    }
+    int end = competitorList.size() - 1;
 
+    while (start < end) {
+        Match m = new Match(competitorList.get(start), competitorList.get(end));
+        matches.add(m);
+        start++;
+        end--;
+    }
+
+    return matches;
+}
