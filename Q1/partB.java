3c8169db2f59a7663a88d25f0e736e664cbd4cad
LazizbekU <lazizbeku@nycstudents.net>
2025-05-14 18:05:18 -0400

    Update partB.java

--- Q1/partB.java
+++ Q1/partB.java
@@ -1,1 +1,12 @@
-
+public int dogWalkShift(int startHour, int endHour) {
+    int money = 0;
+    for (int i = startHour; i <= endHour; i++) {
+        int walked = walkDogs(i);
+        int pay = walked * 5;
+        if (walked == maxDogs || (i >= 9 && i <= 17)) {
+            pay += 3;
+        }
+        money += pay;
+    }
+    return money;
+}
